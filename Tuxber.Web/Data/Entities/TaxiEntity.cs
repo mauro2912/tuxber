54ec49397dd9217b1e652e74d7a7f178d3355932
Mauro2912 <asanchez2912@gmail.com>
2020-02-22 16:58:30 -0500

    Create first controller (TaxiController). Apply first dependency injection about the DB connection.

--- Tuxber.Web/Data/Entities/TaxiEntity.cs
+++ Tuxber.Web/Data/Entities/TaxiEntity.cs
@@ -11,6 +11,7 @@ namespace Tuxber.Web.Data.Entities
         public int Id { get; set; }
 
         [StringLength (6,MinimumLength = 6, ErrorMessage = "The {0} field must have {1} characters. Please review.")]
+        [RegularExpression(@"^([A-Za-z]{3}\d{3})$", ErrorMessage ="The field {0} must starts with 3 characters and finish with three numbers. Please review.")]
         [Required (ErrorMessage ="The field {0} is mandatory.")]
         public string Plaque { get; set; }
     }
