4b4b1d4fa1a9df427de8e632bbbc504d9e937d47
Mauro2912 <asanchez2912@gmail.com>
2020-03-01 00:42:11 -0500

    Building an API. Inprove the TaxisController Add a response class for every model in Common project under folder Models

---
+++ Tuxber.Web/Controllers/API/TaxisController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Tuxber.Web.Data;
+using Tuxber.Web.Data.Entities;
+using Tuxber.Web.Helpers;
+
+namespace Tuxber.Web.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaxisController : ControllerBase
+    {
+        private readonly DataContext _context;
+        private readonly IConverterHelper _converterHelper;
+
+        public TaxisController(DataContext context, IConverterHelper converterHelper)
+        {
+            _context = context;
+            _converterHelper = converterHelper;
+        }
+
+        [HttpGet("{plaque}")]
+        public async Task<IActionResult> GetTaxiEntity([FromRoute] string plaque)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            plaque = plaque.ToUpper();
+            TaxiEntity taxiEntity = await _context.Taxis
+                       .Include(t => t.User)
+                       .Include(t => t.Trips)
+                       .ThenInclude(t => t.TripDetails)
+                       .Include(t => t.Trips)
+                       .ThenInclude(t => t.User)
+                       .FirstOrDefaultAsync(t => t.Plaque == plaque);
+
+            if (taxiEntity == null)
+            {
+                _context.Taxis.Add(new TaxiEntity { Plaque = plaque });
+                await _context.SaveChangesAsync();
+                taxiEntity = await _context.Taxis.FirstOrDefaultAsync(t => t.Plaque == plaque);
+            }
+
+            return Ok(_converterHelper.ToTaxiResponse(taxiEntity));
+        }
+    }
+}
---
+++ Tuxber.Web/Helpers/IConverterHelper.cs
@@ -0,0 +1,10 @@
+﻿using Tuxber.Common.Models;
+using Tuxber.Web.Data.Entities;
+
+namespace Tuxber.Web.Helpers
+{
+    public interface IConverterHelper
+    {
+        TaxiResponse ToTaxiResponse(TaxiEntity taxiEntity);
+    }
+}
